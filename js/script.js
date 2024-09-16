6c37c98c324267ecdeda0fe5c021f77b2228c075
mustafa <mustafaansari@mail.com>
2024-09-17 01:18:03 +0530

    some improvement

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
+
     const menuToggle = document.getElementById('menu-toggle');
     const navLinks = document.getElementById('nav-links');
+    const links = document.querySelectorAll('.nav-box a');
+    const contents = document.querySelectorAll('.event-content');
 
     menuToggle.addEventListener('click', function () {
         navLinks.classList.toggle('active');
     });
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const contentId = this.getAttribute('data-content');
+            contents.forEach(content => {
+                if (content.id === contentId) {
+                    content.style.display = 'block';
+                } else {
+                    content.style.display = 'none';
+                }
+            });
+        });
+    });
 });
+
+
+
