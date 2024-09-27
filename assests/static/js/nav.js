94d5c96f85c7c8c1e754fe068b58f5903ac35086
mustafa <mustafaansari@mail.com>
2024-09-27 10:47:13 +0530

    changes

---
+++ assests/static/js/nav.js
@@ -0,0 +1,5 @@
+// JavaScript to toggle the mobile menu
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active'); // Toggle the 'active' class to show/hide the menu
+});
